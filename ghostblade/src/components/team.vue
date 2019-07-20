8b660fbd83e0f4c0038657447c220d6fbfc4a242
linxd <linxd@wangsu.com>
2019-07-20 16:35:43 +0800

    roll back ghost project

---
+++ ghostblade/src/components/team.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <ul>
+            <li>角色1</li>
+            <li>角色2</li>
+            <li>角色3</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "team"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ ghostblade/src/page/create-role.vue
@@ -0,0 +1,25 @@
+<!--
+这个是一个用来创建角色的文件
+-->
+<template>
+<div>
+
+
+</div>
+</template>
+
+<script>
+    export default {
+
+        methods:{
+
+        },
+        data (){
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
